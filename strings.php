fb9765b0799ca654f648c488fb323ba533b9de60
Laura Kimberly <lauraksp@icloud.com>
2023-01-25 16:29:45 -0300

    vai agora pelo amor de deus

---
+++ strings.php
@@ -0,0 +1,7 @@
+<?php
+
+$texto = "Laura";
+$idade = 21;
+
+/* echo 'Ola mundo, meu nome é ' . $texto . ' e tenho ' . $idade . ' anos.'; */
+echo 'Ola mundo, meu nome é $texto e tenho $idade anos.';
